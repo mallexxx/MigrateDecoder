2009f967501065a26acc065c8a8688d534bfdf1b
Alexey Martemyanov <mallexxx@gmail.com>
2019-01-15 11:16:16 +0700

    Initial Commit

---
+++ ModelMigrate/ModelMigrate.h
@@ -0,0 +1,19 @@
+//
+//  ModelMigrate.h
+//  ModelMigrate
+//
+//  Created by Alexey Martemianov on 15/01/2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ModelMigrate.
+FOUNDATION_EXPORT double ModelMigrateVersionNumber;
+
+//! Project version string for ModelMigrate.
+FOUNDATION_EXPORT const unsigned char ModelMigrateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModelMigrate/PublicHeader.h>
+
+
